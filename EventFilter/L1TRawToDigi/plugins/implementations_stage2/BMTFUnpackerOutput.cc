6c7264d79fd5e4acc2b20290f16888a6eca25422
kpanos <panos.katsoulis@cern.ch>
2018-05-13 20:20:21 +0200

    remove unpacking quality cut for Kalman

--- EventFilter/L1TRawToDigi/plugins/implementations_stage2/BMTFUnpackerOutput.cc
+++ EventFilter/L1TRawToDigi/plugins/implementations_stage2/BMTFUnpackerOutput.cc
@@ -69,7 +69,7 @@ namespace l1t
 	  RegionalMuonCand muCand;
 	  RegionalMuonRawDigiTranslator::fillRegionalMuonCand(muCand, raw_first, raw_secnd, processor, tftype::bmtf);
 
-	  if (muCand.hwQual() == 0)
+	  if (muCand.hwQual() == 0 && !isKalman)
 	    continue;//though away muons with Zero-Quality
 
 	  if (isKalman) {
