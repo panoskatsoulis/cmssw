b901c59223adadbc3f4668f5351e258cb712199d
kpanos <panos.katsoulis@cern.ch>
2018-05-13 20:20:21 +0200

    remove unpacking quality cut for Kalman

--- EventFilter/L1TRawToDigi/python/bmtfDigis_cfi.py
+++ EventFilter/L1TRawToDigi/python/bmtfDigis_cfi.py
@@ -2,14 +2,14 @@ import FWCore.ParameterSet.Config as cms
 
 bmtfDigis = cms.EDProducer(
     "L1TRawToDigi",
-    Setup = cms.string("stage2::BMTFSetup"),
-    InputLabel = cms.InputTag("rawDataCollector"),
-    FedIds = cms.vint32(1376,1377),
-    FWId = cms.uint32(1),
-    lenSlinkHeader = cms.untracked.int32(8),
-    lenSlinkTrailer = cms.untracked.int32(8),
-    lenAMCHeader = cms.untracked.int32(8),
-    lenAMCTrailer = cms.untracked.int32(0),
-    lenAMC13Header = cms.untracked.int32(8),
-    lenAMC13Trailer = cms.untracked.int32(8)
+    Setup = cms.string("stage2::BMTFSetup"),#what module creates the unpackers
+    InputLabel = cms.InputTag("rawDataCollector"),#input to unpack
+    FedIds = cms.vint32(1376,1377),#bmtf feds
+    FWId = cms.uint32(1),#bmtf algo related info
+    lenSlinkHeader = cms.untracked.int32(8),#length of the link's header
+    lenSlinkTrailer = cms.untracked.int32(8),#length of the link's trailer
+    lenAMCHeader = cms.untracked.int32(8),#AMC(MP7) header length
+    lenAMCTrailer = cms.untracked.int32(0),#AMC(MP7) trailer length
+    lenAMC13Header = cms.untracked.int32(8),#AMC13 header length
+    lenAMC13Trailer = cms.untracked.int32(8)#AMC13 trailer length
 )
